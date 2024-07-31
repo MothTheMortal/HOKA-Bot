0deefe03f7aeb7c19773b700863ce6d9dbf53c22
MothTheMortal <96745151+MothTheMortal@users.noreply.github.com>
2024-07-31 18:19:56 +0800

    Update redeem.py

--- src/cogs/redeem.py
+++ src/cogs/redeem.py
@@ -50,12 +50,12 @@ class RedeemCog(commands.Cog):
             data = channel_title
             logMsg = f"**Ticket:** {channel_title}"
 
-        await ctx.followup.send(f"**Created Redeem Code:**\n**Code:** {code}\n**Action:** {action.name}", ephemeral=True)
+        await ctx.followup.send(f"**Created Redeem Code:**\n**Code:** `{code}`\n**Action:** {action.name}", ephemeral=True)
 
         await self.client.createRedeemDocument(code, action.value, uses, ctx.user, data)
 
         logChannel = await ctx.guild.fetch_channel(config.REDEEM_LOG_CHANNEL_ID)
-        embed = discord.Embed(title=f"{ctx.user.name} created a code", description=f"**Code:** {code}\n**Action:** {action.name}\n{logMsg}", color=discord.Colour.blurple())
+        embed = discord.Embed(title=f"{ctx.user.name} created a code", description=f"**Code:** `{code}`\n**Action:** {action.name}\n{logMsg}", color=discord.Colour.blurple())
         await logChannel.send(embed=embed)
 
 
@@ -152,7 +152,7 @@ class RedeemCog(commands.Cog):
 
         text = ""
         for doc in codes:
-            text += f"**Code:** {doc['_id']}\n**Action:** {doc['actionType']}\n**Uses:** {doc['uses']}\n\n"
+            text += f"**Code:** `{doc['_id']}`\n**Action:** {doc['actionType']}\n**Uses:** {doc['uses']}\n\n"
         await ctx.response.send_message(text, ephemeral=True)
 
 
